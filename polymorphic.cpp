5b74c1eb86ab1639f8ff3681fb30764feb90ac24
Wesley W. Terpstra <wesley@sifive.com>
2018-09-04 17:59:24 -0700

    wideml: improve the standard library

--- polymorphic.cpp
+++ polymorphic.cpp
@@ -34,7 +34,26 @@ static void prim_eq(void *data, const std::vector<Value*> &args, Action *complet
   resume(completion, cmp == 0 ? prim_true : prim_false);
 }
 
+static void prim_cmp(void *data, const std::vector<Value*> &args, Action *completion) {
+  EXPECT_ARGS(2);
+  int cmp;
+  if (args[0]->type == Integer::type) {
+    Integer *arg0 = GET_INTEGER(0);
+    Integer *arg1 = GET_INTEGER(1);
+    cmp = mpz_cmp(arg0->value, arg1->value);
+  } else {
+    String *arg0 = GET_STRING(0);
+    String *arg1 = GET_STRING(1);
+    cmp = arg0->value.compare(arg1->value);
+  }
+  // Normalize it
+  Integer *out = new Integer;
+  mpz_set_si(out->value, (cmp < 0) ? -1 : (cmp > 0) ? 1 : 0);
+  resume(completion, out);
+}
+
 void prim_register_polymorphic(PrimMap &pmap) {
   pmap["lt"].first = prim_lt;
   pmap["eq"].first = prim_eq;
+  pmap["cmp"].first = prim_cmp;
 }
