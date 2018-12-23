97a2d8dba01383dae6ab8178d1030c9350b4d909
Wesley W. Terpstra <wesley@sifive.com>
2018-12-23 10:44:12 +0100

    wake: add a primitive that prints type information

--- src/bind.cpp
+++ src/bind.cpp
@@ -486,7 +486,7 @@ static bool explore(Expr *expr, const PrimMap &pmap, NameBinding *binding) {
     } else {
       prim->fn   = i->second.fn;
       prim->data = i->second.data;
-      bool ok = i->second.type(i->second.data, args, &prim->typeVar);
+      bool ok = i->second.type(&prim->data, args, &prim->typeVar);
       if (!ok) std::cerr << "Primitive reference "
         << prim->name << " has wrong type signature at "
         << prim->location << std::endl;
