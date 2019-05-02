db80dd48cd428cd1b4e195fc1bc2993c901d46cc
Wesley W. Terpstra <wesley@sifive.com>
2019-05-02 16:38:07 -0700

    wake: point at the local LICENSE.Apache2 file

--- src/parser.h
+++ src/parser.h
@@ -3,7 +3,8 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You should have received a copy of LICENSE.Apache2 along with
+ * this software. If not, you may obtain a copy at
  *
  *    https://www.apache.org/licenses/LICENSE-2.0
  *
