28bcdbec6277b261b0b8af2df6d352906b8a3510
Wesley W. Terpstra <wesley@sifive.com>
2019-07-30 15:14:20 -0700

    wake: consistently escape shell command output (#224)

---
+++ common/shell.cpp
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2019 SiFive, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You should have received a copy of LICENSE.Apache2 along with
+ * this software. If not, you may obtain a copy at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "shell.h"
+#include <sstream>
+#include <string.h>
+
+// There are more special than non-special characters with the shell.
+static bool not_special(char c) {
+  switch (c) {
+    case 'A':
+    case 'B':
+    case 'C':
+    case 'D':
+    case 'E':
+    case 'F':
+    case 'G':
+    case 'H':
+    case 'I':
+    case 'J':
+    case 'K':
+    case 'L':
+    case 'M':
+    case 'N':
+    case 'O':
+    case 'P':
+    case 'Q':
+    case 'R':
+    case 'S':
+    case 'T':
+    case 'U':
+    case 'V':
+    case 'W':
+    case 'X':
+    case 'Y':
+    case 'Z':
+
+    case 'a':
+    case 'b':
+    case 'c':
+    case 'd':
+    case 'e':
+    case 'f':
+    case 'g':
+    case 'h':
+    case 'i':
+    case 'j':
+    case 'k':
+    case 'l':
+    case 'm':
+    case 'n':
+    case 'o':
+    case 'p':
+    case 'q':
+    case 'r':
+    case 's':
+    case 't':
+    case 'u':
+    case 'v':
+    case 'w':
+    case 'x':
+    case 'y':
+    case 'z':
+
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+
+    case ':':
+    case '/':
+    case '.':
+    case ',':
+    case '+':
+    case '-':
+    case '_':
+    case '%':
+    case '@':
+    case '^':
+    case '=':
+      return true;
+    default: 
+      return false;
+  }
+}
+
+std::string shell_escape(const char *x) {
+  // check if we need escaping at all
+  const char *ok;
+  for (ok = x; not_special(*ok); ++ok) { }
+  if (!*ok) return x;
+
+  std::stringstream s;
+  s << "'";
+
+  while (const char *e = strchr(x, '\'')) {
+    s.write(x, e-x);
+    s << "'\\''";
+    x = e+1;
+  }
+
+  s << x << "'";
+  return s.str();
+}
