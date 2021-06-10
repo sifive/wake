ff19ea24f0b81da8d04e677d19d8c2823c2c5e88
Wesley W. Terpstra <wesley@sifive.com>
2021-06-10 14:11:29 -0700

    common: add a wrapper around non-portable ns modification time

---
+++ common/mtime.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 SiFive, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You should have received a copy of LICENSE.Apache2 along with
+ * this software. If not, you may obtain a copy at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Open Group Base Specifications Issue 7
+#define _XOPEN_SOURCE 700
+
+// OS/X only makes st_mtimespec available as an extension
+#define _DARWIN_C_SOURCE 1
+
+#include <sys/stat.h>
+#include "mtime.h"
+
+#ifdef __APPLE__
+#define st_mtim st_mtimespec
+#endif
+
+int64_t getmtime_ns(const char *file) {
+  struct stat sbuf;
+  int ret = stat(file, &sbuf);
+  if (ret == -1) return -1;
+  return sbuf.st_mtim.tv_nsec*INT64_C(1000000000) + sbuf.st_mtim.tv_sec;
+}
