ce696fbe432a339395e445be101f401145ea4829
Wesley W. Terpstra <wesley@sifive.com>
2021-06-10 17:05:12 -0700

    common: add a utimens wrapper
    This is a standard function, but missing on some operating systems.

---
+++ common/utimens.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 SiFive, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You should have received a copy of LICENSE.Apache2 along with
+ * this software. If not, you may obtain a copy at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Open Group Base Specifications Issue 7
+#define _XOPEN_SOURCE 700
+
+// OS/X only makes utimensat available as an extension
+#define _DARWIN_C_SOURCE 1
+
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "utimens.h"
+
+int wake_utimensat(int dirfd, const char *path, const struct timespec ts[2]) {
+	return utimensat(dirfd, path, ts, AT_SYMLINK_NOFOLLOW);
+}
