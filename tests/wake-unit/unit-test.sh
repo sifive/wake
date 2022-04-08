b2b3bc72506c9a61f0b60aab26c75571a4db596f
Jake Ehrlich <jake.ehrlich@sifive.com>
2022-04-08 12:40:50 -0700

    Remove another reference to buildWakeUnit

--- tests/wake-unit/unit-test.sh
+++ tests/wake-unit/unit-test.sh
@@ -4,5 +4,5 @@ set -e
 
 # TODO: Change this command to something where `isatty` is true. For now
 # its fine if we don't worry about that
-#TERM=xterm-256color script --return --quiet -c "$2 --no-color" /dev/null
-$2
+#TERM=xterm-256color script --return --quiet -c "$1 --no-color" /dev/null
+$1
