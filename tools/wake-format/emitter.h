5d4177f1e5ea0507cb3480e6491800e9c87010e4
Ashley Coleman <ashley.coleman@sifive.com>
2022-07-20 17:37:33 -0500

    wake-format: Add tree walk boilerplate code (#950)
    Adds boilerplate functions for tree walk code. Simplifies later reviews.

--- tools/wake-format/emitter.h
+++ tools/wake-format/emitter.h
@@ -50,14 +50,52 @@ class Emitter {
 
  private:
   // Top level tree walk. Dispatches out the calls for various nodes
-  wcl::rope walk_top(ctx_t ctx, CSTElement node);
+  wcl::rope walk(ctx_t ctx, CSTElement node);
 
   wcl::optional<wcl::rope> walk_node(ctx_t ctx, CSTElement node);
   wcl::rope walk_token(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_rhs(ctx_t ctx, CSTElement node);
 
-  wcl::rope walk_block(ctx_t ctx, CSTElement node);
-  wcl::rope walk_def(ctx_t ctx, CSTElement node);
-  wcl::rope walk_apply(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_apply(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_arity(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_ascribe(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_binary(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_block(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_case(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_data(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_def(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_export(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_flag_export(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_flag_global(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_guard(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_hole(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_identifier(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_ideq(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_if(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_import(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_interpolate(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_kind(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_lambda(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_literal(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_match(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_op(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_package(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_paren(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_prim(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_publish(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_require(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_req_else(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_subscribe(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_target(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_target_args(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_top(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_topic(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_tuple(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_tuple_elt(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_unary(ctx_t ctx, CSTElement node);
+  wcl::optional<wcl::rope> walk_error(ctx_t ctx, CSTElement node);
+
+  wcl::optional<wcl::rope> walk_placeholder(ctx_t ctx, CSTElement node);
 
   // Emits a newline and any indentation needed
   wcl::rope newline(ctx_t ctx);
@@ -65,13 +103,15 @@ class Emitter {
   // Emits a space character `count` times
   wcl::rope space(uint8_t count = 1);
 
-  // TODO: memoize this function
-  // returns the subtree with all newlines and indentation removed
-  // if possible, None otherwise
+  // tries to format the subree in a flat context,
+  // returns None if it fails
   wcl::optional<wcl::rope> flat(ctx_t ctx, CSTElement node);
 
-  // Attempts to flatten `node` with flat. On failure returns the full node
-  wcl::rope try_flat(ctx_t ctx, CSTElement node);
+  // TODO: memoize this function
+  // tries to format the subtree in a flat context, if it fails
+  // then None is returned when calling from a flat context and
+  // full is returned otherwise.
+  wcl::optional<wcl::rope> flat_or(ctx_t ctx, CSTElement node);
 
   const uint8_t space_per_indent = 4;
   const uint8_t max_column_width = 100;
