11a4c269359a1c51b7848637e06a30be5efef6c2
Ashley Coleman <ash123125@gmail.com>
2022-08-24 09:24:13 -0700

    sh compliant test script

--- tests/wake-format/failing-input/fail.sh
+++ tests/wake-format/failing-input/fail.sh
@@ -1,19 +1,17 @@
-#! /bin/bash
-
-function cleanup {
-  rm leading-comment-indent.wake.tmp.00000000000000000000000000000000
-  rm trailing-comment.wake.tmp.00000000000000000000000000000000
-}
-trap cleanup EXIT
+#! /bin/sh
 
 # Leading comment indent test
 if "${1}/wake-format" --no-rng leading-comment-indent.wake; then
+  rm leading-comment-indent.wake.tmp.00000000000000000000000000000000
   exit 0
 fi
 
 # Trailing comment test
 if "${1}/wake-format" --no-rng trailing-comment.wake; then
+  rm trailing-comment.wake.tmp.00000000000000000000000000000000
   exit 0
 fi
 
+rm leading-comment-indent.wake.tmp.00000000000000000000000000000000
+rm trailing-comment.wake.tmp.00000000000000000000000000000000
 exit 1
