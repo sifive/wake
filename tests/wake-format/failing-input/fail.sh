f9fdb15810b1799249bf44241762e5a188a3d86a
Ashley Coleman <ash123125@gmail.com>
2022-08-24 09:25:06 -0700

    cleanup

--- tests/wake-format/failing-input/fail.sh
+++ tests/wake-format/failing-input/fail.sh
@@ -8,6 +8,7 @@ fi
 
 # Trailing comment test
 if "${1}/wake-format" --no-rng trailing-comment.wake; then
+  rm leading-comment-indent.wake.tmp.00000000000000000000000000000000
   rm trailing-comment.wake.tmp.00000000000000000000000000000000
   exit 0
 fi
