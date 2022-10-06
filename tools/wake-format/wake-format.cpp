5625e27c5360d850fc3903001df5b28efa32a8e6
Ashley Coleman <ash123125@gmail.com>
2022-10-06 13:02:41 -0700

    ci test

--- tools/wake-format/wake-format.cpp
+++ tools/wake-format/wake-format.cpp
@@ -51,7 +51,7 @@ class TerminalReporter : public DiagnosticReporter {
   bool errors;
   bool warnings;
 
-  void report_to_stderr() { std::cerr << "Start<" << ostream.str() << ">End"; }
+  void report_to_stderr() { std::cerr << ostream.str(); }
 
  private:
   std::stringstream ostream;
