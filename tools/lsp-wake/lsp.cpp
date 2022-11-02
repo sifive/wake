02e7f734e0fd94bace7045d9b8eebc0b433cd1b1
Ashley Coleman <ashley.coleman@sifive.com>
2022-11-02 11:50:03 -0700

    Fix error message when running lsp in neovim (#1057)
    * Fix error message when running lsp in neovim
    * cleanup

--- tools/lsp-wake/lsp.cpp
+++ tools/lsp-wake/lsp.cpp
@@ -301,11 +301,18 @@ class LSPServer {
   static void sendMessage(const JAST &message) {
     std::stringstream str;
     str << message;
-    str.seekg(0, std::ios::end);
-    size_t length = str.tellg();
-    str.seekg(0, std::ios::beg);
-    std::cout << contentLength << (length + 2) << "\r\n\r\n";
-    std::cout << str.rdbuf() << "\r\n" << std::flush;
+
+    std::string msg = str.str();
+
+    if (msg == "{}") {
+#ifdef CERR_DEBUG
+      std::cerr << "Throwing away empty response message" << std::endl;
+#endif
+      return;
+    }
+
+    std::cout << contentLength << (msg.size() + 2) << "\r\n\r\n";
+    std::cout << msg << "\r\n" << std::flush;
   }
 
   MethodResult initialize(const JAST &receivedMessage) {
