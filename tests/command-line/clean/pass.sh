b31752f896eb06eb6e8dcf7b29438c0fe1d67292
Ashley Coleman <ash123125@gmail.com>
2023-02-10 15:11:27 -0800

    bisect

--- tests/command-line/clean/pass.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-WAKE="${1:+$1/wake}"
-"${WAKE:-wake}" -q --init .
-"${WAKE:-wake}" -q -x 'write "bug" ""'
-"${WAKE:-wake}" --clean
--- tests/command-line/qv/fail.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-WAKE="${1:+$1/wake}"
-"${WAKE:-wake}" -qv -x Unit
