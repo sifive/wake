d447999dae43b96ae26ef00339e1e7da727c31bf
Ashley Coleman <ash123125@gmail.com>
2023-05-10 15:19:46 -0700

    more

--- tests/runtime/symlinks/pass.sh
+++ tests/runtime/symlinks/pass.sh
@@ -6,23 +6,23 @@ WAKE="${1:+$1/wake}"
 WAKE="${WAKE:-wake}"
 
 rm -f datFile badLink goodLink wake.db
-DEBUGNAME=wake.test.symlink.log "${WAKE}" -v test
-DEBUGNAME=wake.test.symlink.log "${WAKE}" -v test
+DEBUGNAME=wake.test.symlink1.log "${WAKE}" -v test
+DEBUGNAME=wake.test.symlink2.log "${WAKE}" -v test
 
 rm wake.db
-DEBUGNAME=wake.test.symlink.log "${WAKE}" -v test
+DEBUGNAME=wake.test.symlink3.log "${WAKE}" -v test
 
 rm -f badLink goodLink
 ln -s badFile badLink
 ln -s datFile goodLink
-DEBUGNAME=wake.test.symlink.log "${WAKE}" -v test
-DEBUGNAME=wake.test.symlink.log "${WAKE}" -v test
+DEBUGNAME=wake.test.symlink4.log "${WAKE}" -v test
+DEBUGNAME=wake.test.symlink5.log "${WAKE}" -v test
 
 rm -f datFile badLink goodLink wake.db
 ln -s badFile badLink
 ln -s datFile goodLink
-DEBUGNAME=wake.test.symlink.log "${WAKE}" -v test
-DEBUGNAME=wake.test.symlink.log "${WAKE}" -v test
+DEBUGNAME=wake.test.symlink6.log "${WAKE}" -v test
+DEBUGNAME=wake.test.symlink7.log "${WAKE}" -v test
 
 rm -f datFile badLink goodLink .fuse.log
 echo "Symlinks work!"
