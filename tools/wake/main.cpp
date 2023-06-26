3fe6ea6ecd92bacfdd11e4500f574a54b85868ab
Ashley Coleman <ash123125@gmail.com>
2023-06-26 09:42:34 -0700

    Revert "Revert "Add logInteractive, remove echo from no-tty, update docs (#1302)" (#1310)"
    This reverts commit 54e92e013d280d4032540a0a0874fbbfad6bc886.

--- tools/wake/main.cpp
+++ tools/wake/main.cpp
@@ -746,13 +746,22 @@ int main(int argc, char **argv) {
   if (notype) return (ok && !terminalReporter.errors) ? 0 : 1;
 
   /* Setup logging streams */
-  if (noexecute && !clo.fd1) clo.fd1 = "error";
-  if (clo.debug && !clo.fd1) clo.fd1 = "debug,info,echo,report,warning,error";
-  if (clo.verbose && !clo.fd1) clo.fd1 = "info,echo,report,warning,error";
-  if (clo.quiet && !clo.fd1) clo.fd1 = "error";
-  if (!clo.tty && !clo.fd1) clo.fd1 = "echo,report,warning,error";
-  if (!clo.fd1) clo.fd1 = "report,warning,error";
-  if (!clo.fd2) clo.fd2 = "error";
+
+  if (clo.tty) {
+    if (noexecute && !clo.fd1) clo.fd1 = "error";
+    if (clo.debug && !clo.fd1) clo.fd1 = "debug,info,echo,interactive,report,warning,error";
+    if (clo.verbose && !clo.fd1) clo.fd1 = "info,echo,interactive,report,warning,error";
+    if (clo.quiet && !clo.fd1) clo.fd1 = "error";
+    if (!clo.fd1) clo.fd1 = "interactive,report,warning,error";
+    if (!clo.fd2) clo.fd2 = "error";
+  } else {
+    if (noexecute && !clo.fd1) clo.fd1 = "error";
+    if (clo.debug && !clo.fd1) clo.fd1 = "debug,info,echo,report,warning,error";
+    if (clo.verbose && !clo.fd1) clo.fd1 = "info,echo,report,warning,error";
+    if (clo.quiet && !clo.fd1) clo.fd1 = "error";
+    if (!clo.fd1) clo.fd1 = "report,warning,error";
+    if (!clo.fd2) clo.fd2 = "error";
+  }
 
   status_set_bulk_fd(1, clo.fd1);
   status_set_bulk_fd(2, clo.fd2);
