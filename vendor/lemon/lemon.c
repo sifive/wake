99fe2c1678f22f03677a05dc474268b50f180914
Ashley Coleman <ash123125@gmail.com>
2023-06-29 12:14:21 -0700

    Emit cpp files from lemon

--- vendor/lemon/lemon.c
+++ vendor/lemon/lemon.c
@@ -4311,7 +4311,7 @@ void ReportTable(
 
   in = tplt_open(lemp);
   if( in==0 ) return;
-  out = file_open(lemp,".c","wb");
+  out = file_open(lemp,".cpp","wb");
   if( out==0 ){
     fclose(in);
     return;
