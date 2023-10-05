6a28a2ed3794ea32e70076a9b6b1d2cab086a772
Ashley Coleman <ashley.coleman@sifive.com>
2023-10-05 13:46:04 -0700

    Don't exit from manifest error in LSP

--- tools/lsp-wake/astree.cpp
+++ tools/lsp-wake/astree.cpp
@@ -62,7 +62,7 @@ void ASTree::diagnoseProject(const std::function<void(FileDiagnostics &)> &proce
   comments.clear();
 
   bool enumok = true;
-  auto allFiles = find_all_wakefiles(enumok, true, false, absLibDir, absWorkDir, stdout);
+  auto allFiles = find_all_wakefiles(enumok, true, false, absLibDir, absWorkDir, stdout, false);
 
   std::map<std::string, std::vector<Diagnostic>> diagnostics;
   LSPReporter lspReporter(diagnostics, allFiles);
