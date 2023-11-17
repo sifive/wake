fabb922290b08c62b75509e326a592d43e4fd3f0
Ashley Coleman <ashley.coleman@sifive.com>
2023-11-16 16:26:10 -0800

    Code cleanup & format

--- rust/rsc/src/rsc/add_job.rs
+++ rust/rsc/src/rsc/add_job.rs
@@ -1,13 +1,12 @@
+use crate::types::AddJobPayload;
 use axum::{http::StatusCode, Json};
 use entity::prelude::{OutputDir, OutputFile, OutputSymlink, VisibleFile};
 use entity::{job, output_dir, output_file, output_symlink, visible_file};
 use sea_orm::{
-    ActiveModelTrait, ActiveValue, DatabaseConnection, DbErr, EntityTrait, TransactionTrait,
+    ActiveModelTrait, ActiveValue::*, DatabaseConnection, DbErr, EntityTrait, TransactionTrait,
 };
 use std::sync::Arc;
 use tracing;
-
-use crate::types::AddJobPayload;
 
 #[tracing::instrument]
 pub async fn add_job(
@@ -21,22 +20,22 @@ pub async fn add_job(
     let output_symlinks = payload.output_symlinks;
     let output_dirs = payload.output_dirs;
     let insert_job = job::ActiveModel {
-        id: ActiveValue::NotSet,
-        hash: ActiveValue::Set(hash.clone().into()),
-        cmd: ActiveValue::Set(payload.cmd),
-        env: ActiveValue::Set(payload.env.as_bytes().into()),
-        cwd: ActiveValue::Set(payload.cwd),
-        stdin: ActiveValue::Set(payload.stdin),
-        is_atty: ActiveValue::Set(payload.is_atty),
-        hidden_info: ActiveValue::Set(payload.hidden_info),
-        stdout: ActiveValue::Set(payload.stdout),
-        stderr: ActiveValue::Set(payload.stderr),
-        status: ActiveValue::Set(payload.status),
-        runtime: ActiveValue::Set(payload.runtime),
-        cputime: ActiveValue::Set(payload.cputime),
-        memory: ActiveValue::Set(payload.memory as i64),
-        i_bytes: ActiveValue::Set(payload.ibytes as i64),
-        o_bytes: ActiveValue::Set(payload.obytes as i64),
+        id: NotSet,
+        hash: Set(hash.clone().into()),
+        cmd: Set(payload.cmd),
+        env: Set(payload.env.as_bytes().into()),
+        cwd: Set(payload.cwd),
+        stdin: Set(payload.stdin),
+        is_atty: Set(payload.is_atty),
+        hidden_info: Set(payload.hidden_info),
+        stdout: Set(payload.stdout),
+        stderr: Set(payload.stderr),
+        status: Set(payload.status),
+        runtime: Set(payload.runtime),
+        cputime: Set(payload.cputime),
+        memory: Set(payload.memory as i64),
+        i_bytes: Set(payload.ibytes as i64),
+        o_bytes: Set(payload.obytes as i64),
     };
 
     // Now perform the insert as a single transaction
@@ -47,61 +46,54 @@ pub async fn add_job(
                 let job = insert_job.save(txn).await?;
                 let job_id = job.id.unwrap();
 
-                let visible_files: Vec<_> = vis
-                    .into_iter()
-                    .map(|vis_file| visible_file::ActiveModel {
-                        id: ActiveValue::NotSet,
-                        path: ActiveValue::Set(vis_file.path),
-                        hash: ActiveValue::Set(vis_file.hash.into()),
-                        job_id: ActiveValue::Set(job_id),
-                    })
-                    .collect();
+                let visible_files = vis.into_iter().map(|vis_file| visible_file::ActiveModel {
+                    id: NotSet,
+                    path: Set(vis_file.path),
+                    hash: Set(vis_file.hash.into()),
+                    job_id: Set(job_id),
+                });
 
                 VisibleFile::insert_many(visible_files)
                     .on_empty_do_nothing()
                     .exec(txn)
                     .await?;
 
-                let out_files: Vec<_> = output_files
+                let out_files = output_files
                     .into_iter()
                     .map(|out_file| output_file::ActiveModel {
-                        id: ActiveValue::NotSet,
-                        path: ActiveValue::Set(out_file.path),
-                        hash: ActiveValue::Set(out_file.hash.into()),
-                        mode: ActiveValue::Set(out_file.mode),
-                        job_id: ActiveValue::Set(job_id),
-                    })
-                    .collect();
+                        id: NotSet,
+                        path: Set(out_file.path),
+                        hash: Set(out_file.hash.into()),
+                        mode: Set(out_file.mode),
+                        job_id: Set(job_id),
+                    });
 
                 OutputFile::insert_many(out_files)
                     .on_empty_do_nothing()
                     .exec(txn)
                     .await?;
 
-                let out_symlinks: Vec<_> = output_symlinks
-                    .into_iter()
-                    .map(|out_symlink| output_symlink::ActiveModel {
-                        id: ActiveValue::NotSet,
-                        path: ActiveValue::Set(out_symlink.path),
-                        content: ActiveValue::Set(out_symlink.content),
-                        job_id: ActiveValue::Set(job_id),
-                    })
-                    .collect();
+                let out_symlinks =
+                    output_symlinks
+                        .into_iter()
+                        .map(|out_symlink| output_symlink::ActiveModel {
+                            id: NotSet,
+                            path: Set(out_symlink.path),
+                            content: Set(out_symlink.content),
+                            job_id: Set(job_id),
+                        });
 
                 OutputSymlink::insert_many(out_symlinks)
                     .on_empty_do_nothing()
                     .exec(txn)
                     .await?;
 
-                let dirs: Vec<_> = output_dirs
-                    .into_iter()
-                    .map(|dir| output_dir::ActiveModel {
-                        id: ActiveValue::NotSet,
-                        path: ActiveValue::Set(dir.path),
-                        mode: ActiveValue::Set(dir.mode),
-                        job_id: ActiveValue::Set(job_id),
-                    })
-                    .collect();
+                let dirs = output_dirs.into_iter().map(|dir| output_dir::ActiveModel {
+                    id: NotSet,
+                    path: Set(dir.path),
+                    mode: Set(dir.mode),
+                    job_id: Set(job_id),
+                });
 
                 OutputDir::insert_many(dirs)
                     .on_empty_do_nothing()
