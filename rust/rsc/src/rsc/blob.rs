69eb08545e2c2588c58bbfe7e1eb2000617f6547
Ashley Coleman <ashley.coleman@sifive.com>
2023-12-12 14:53:15 -0700

    Use a trait to define a blob store

--- rust/rsc/src/rsc/blob.rs
+++ rust/rsc/src/rsc/blob.rs
@@ -1,8 +1,9 @@
 use crate::config::RSCConfig;
 use crate::types::GetUploadUrlResponse;
+use async_trait::async_trait;
 use axum::{body::Bytes, extract::Multipart, http::StatusCode, BoxError, Json};
 use data_encoding::BASE64URL;
-use futures::{Stream, TryStreamExt};
+use futures::{Stream, TryStream, TryStreamExt};
 use rand_core::{OsRng, RngCore};
 use sea_orm::DatabaseConnection;
 use std::sync::Arc;
@@ -11,37 +12,40 @@ use tokio::io::BufWriter;
 use tokio_util::io::StreamReader;
 use tracing;
 
+#[async_trait]
+pub trait BlobStore {
+    async fn stream<A, B>(&self, reader: StreamReader<A, B>) -> Result<String, std::io::Error>
+    where
+        StreamReader<A, B>: tokio::io::AsyncRead + std::marker::Send;
+}
+
+pub struct LocalBlobStore {
+    pub root: String,
+}
+
+#[async_trait]
+impl BlobStore for LocalBlobStore {
+    async fn stream<A, B>(&self, reader: StreamReader<A, B>) -> Result<String, std::io::Error>
+    where
+        StreamReader<A, B>: tokio::io::AsyncRead + std::marker::Send,
+    {
+        futures::pin_mut!(reader);
+
+        let name = create_temp_filename();
+        let path = std::path::Path::new(&self.root).join(name.clone());
+        let mut file = BufWriter::new(File::create(path).await?);
+
+        tokio::io::copy(&mut reader, &mut file).await?;
+
+        Ok(name)
+    }
+}
+
 fn create_temp_filename() -> String {
     let mut key = [0u8; 16];
     OsRng.fill_bytes(&mut key);
     // URL must be used as files can't contain /
     BASE64URL.encode(&key)
-}
-
-async fn stream_to_file<S, E>(parent: &str, stream: S) -> Result<String, (StatusCode, String)>
-where
-    S: Stream<Item = Result<Bytes, E>>,
-    E: Into<BoxError>,
-{
-    async {
-        // Convert the stream into an `AsyncRead`.
-        let body_with_io_error =
-            stream.map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err));
-        let body_reader = StreamReader::new(body_with_io_error);
-        futures::pin_mut!(body_reader);
-
-        // Create the file. `File` implements `AsyncWrite`.
-        let name = create_temp_filename();
-        let path = std::path::Path::new(parent).join(name.clone());
-        let mut file = BufWriter::new(File::create(path).await?);
-
-        // Copy the body into the file.
-        tokio::io::copy(&mut body_reader, &mut file).await?;
-
-        Ok::<String, std::io::Error>(name)
-    }
-    .await
-    .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))
 }
 
 #[tracing::instrument]
@@ -56,16 +60,20 @@ pub async fn create_blob(
     _conn: Arc<DatabaseConnection>,
     config: Arc<RSCConfig>,
 ) -> (StatusCode, String) {
-    while let Ok(Some(field)) = multipart.next_field().await {
-        let Some(ref local_store) = config.local_store else {
-            return (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Invalid configuration".into(),
-            );
-        };
+    let Some(local_store) = config.local_store.clone() else {
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Invalid configuration".into(),
+        );
+    };
 
-        if let Err(inner) = stream_to_file(local_store, field).await {
-            return inner;
+    let store = LocalBlobStore { root: local_store };
+
+    while let Ok(Some(field)) = multipart.next_field().await {
+        let body = field.map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err));
+        match store.stream(StreamReader::new(body)).await {
+            Ok(key) => println!("{:?}", key),
+            Err(msg) => return (StatusCode::INTERNAL_SERVER_ERROR, msg.to_string()),
         }
     }
 
