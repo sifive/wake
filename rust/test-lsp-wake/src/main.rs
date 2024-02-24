9fe5deac9344905ba554c7ca39cfc8ed9b60e8aa
Ashley Coleman <ashley.coleman@sifive.com>
2024-02-23 16:15:21 -0800

    refresh PR to current standards

--- rust/test-lsp-wake/src/main.rs
+++ rust/test-lsp-wake/src/main.rs
@@ -1,7 +1,9 @@
 use tower_lsp::lsp_types::*;
 
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, Lines};
-use tokio::process::{ChildStdin, ChildStdout, Command};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::process::{ChildStdin, Command};
+use tokio::sync::mpsc::{channel, Receiver};
+use tokio::time::timeout;
 
 use tower_lsp::lsp_types::notification::{
     DidChangeTextDocument, DidOpenTextDocument, Exit, Initialized, PublishDiagnostics,
@@ -29,20 +31,6 @@ where
         .finish()
 }
 
-async fn write_request<R>(
-    stdin: &mut ChildStdin,
-    id: tower_lsp::jsonrpc::Id,
-    params: R::Params,
-) -> Result<usize, std::io::Error>
-where
-    R: tower_lsp::lsp_types::request::Request,
-    R::Params: serde::Serialize,
-    R::Result: serde::de::DeserializeOwned,
-{
-    let req = make_request::<R>(id, params);
-    stdin.write(jrpc_serialize(req).as_bytes()).await
-}
-
 fn make_notification<R>(params: R::Params) -> tower_lsp::jsonrpc::Request
 where
     R: tower_lsp::lsp_types::notification::Notification,
@@ -53,103 +41,183 @@ where
         .finish()
 }
 
-async fn write_notification<R>(
-    stdin: &mut ChildStdin,
-    params: R::Params,
-) -> Result<usize, std::io::Error>
-where
-    R: tower_lsp::lsp_types::notification::Notification,
-    R::Params: serde::Serialize,
-{
-    let req = make_notification::<R>(params);
-    stdin.write(jrpc_serialize(req).as_bytes()).await
+struct LSPServer {
+    send: ChildStdin,
+    receive: Receiver<String>,
+    peek: Option<String>,
 }
 
-async fn read_notification<R>(
-    reader: &mut Lines<BufReader<ChildStdout>>,
-) -> std::io::Result<R::Params>
-where
-    R: tower_lsp::lsp_types::notification::Notification,
-    R::Params: serde::Serialize,
-{
-    let Some(_length) = reader.next_line().await? else {
-        todo!();
-    };
-    let Some(_end_header) = reader.next_line().await? else {
-        todo!();
-    };
-    let Some(json) = reader.next_line().await? else {
-        todo!();
-    };
+impl LSPServer {
+    fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        let mut child = Command::new("../../lib/wake/lsp-wake")
+            .stdout(Stdio::piped())
+            .stdin(Stdio::piped())
+            .spawn()?;
 
-    let res: tower_lsp::jsonrpc::Request = serde_json::from_str(&json).unwrap();
-    let value = res.params().unwrap();
-    let inner: R::Params = serde_json::from_value(value.clone()).unwrap();
-    return Ok(inner);
-}
+        let stdin = child
+            .stdin
+            .take()
+            .expect("failed to take child stdin handle");
 
-async fn read_response<R>(reader: &mut Lines<BufReader<ChildStdout>>) -> std::io::Result<R::Result>
-where
-    R: tower_lsp::lsp_types::request::Request,
-    R::Params: serde::Serialize,
-    R::Result: serde::de::DeserializeOwned,
-{
-    let _length = reader.next_line().await?;
-    let _end_header = reader.next_line().await?;
-    let Some(json) = reader.next_line().await? else {
-        todo!();
-    };
+        let stdout = child
+            .stdout
+            .take()
+            .expect("failed to take child stdout handle");
 
-    let res: tower_lsp::jsonrpc::Response = serde_json::from_str(&json).unwrap();
-    let value = res.result().unwrap();
-    let inner: R::Result = serde_json::from_value(value.clone()).unwrap();
-    return Ok(inner);
-}
+        // Spawn a task to constantly read messages from the server
+        let (tx, rx) = channel(100);
+        tokio::spawn(async move {
+            let mut reader = BufReader::new(stdout).lines();
+            loop {
+                let Ok(_length) = reader.next_line().await else {
+                    panic!("Failed to read length from msg");
+                };
+                let Ok(_end_header) = reader.next_line().await else {
+                    panic!("Failed to read end header from msg");
+                };
+                let Ok(Some(json)) = reader.next_line().await else {
+                    panic!("Failed to read json from msg");
+                };
 
-async fn exchange_request_response<R>(
-    stdin: &mut ChildStdin,
-    reader: &mut Lines<BufReader<ChildStdout>>,
-    id: tower_lsp::jsonrpc::Id,
-    params: R::Params,
-) -> std::io::Result<R::Result>
-where
-    R: tower_lsp::lsp_types::request::Request,
-    R::Params: serde::Serialize,
-    R::Result: serde::de::DeserializeOwned,
-{
-    write_request::<R>(stdin, id, params).await?;
-    read_response::<R>(reader).await
+                if let Err(msg) = tx.send(json).await {
+                    panic!("Dropped msg tx: {}", msg);
+                }
+            }
+        });
+
+        Ok(Self {
+            send: stdin,
+            receive: rx,
+            peek: None,
+        })
+    }
+
+    async fn write_request<R>(
+        &mut self,
+        id: tower_lsp::jsonrpc::Id,
+        params: R::Params,
+    ) -> Result<usize, std::io::Error>
+    where
+        R: tower_lsp::lsp_types::request::Request,
+        R::Params: serde::Serialize,
+        R::Result: serde::de::DeserializeOwned,
+    {
+        let req = make_request::<R>(id, params);
+        self.send.write(jrpc_serialize(req).as_bytes()).await
+    }
+
+    async fn write_notification<R>(&mut self, params: R::Params) -> Result<usize, std::io::Error>
+    where
+        R: tower_lsp::lsp_types::notification::Notification,
+        R::Params: serde::Serialize,
+    {
+        let req = make_notification::<R>(params);
+        self.send.write(jrpc_serialize(req).as_bytes()).await
+    }
+
+    async fn expect_one<R>(&mut self) -> R::Result
+    where
+        R: tower_lsp::lsp_types::request::Request,
+        R::Params: serde::Serialize,
+        R::Result: serde::de::DeserializeOwned,
+    {
+        let json = match self.peek.take() {
+            Some(json) => json,
+            None => {
+                let Ok(Some(json)) =
+                    timeout(std::time::Duration::from_millis(1000), self.receive.recv()).await
+                else {
+                    panic!(
+                        "expect_one<{}> failed! No server response within the deadline.",
+                        R::METHOD
+                    );
+                };
+                json
+            }
+        };
+
+        let res: tower_lsp::jsonrpc::Response = match serde_json::from_str(&json) {
+            Ok(res) => res,
+            Err(msg) => panic!(
+                "expect_one<{}> failed! Could not parse. Reason: {}. In: {}",
+                R::METHOD,
+                msg,
+                json
+            ),
+        };
+
+        let value = match res.into_parts() {
+            (_, Ok(value)) => value,
+            (_, Err(msg)) => panic!(
+                "expect_one<{}> failed! jsonrpc error. Reason: {}. In: {}",
+                R::METHOD,
+                msg,
+                json
+            ),
+        };
+
+        let inner: R::Result = match serde_json::from_value(value.clone()) {
+            Ok(res) => res,
+            Err(msg) => panic!(
+                "expect_one<{}> failed! Could not parse. Reason: {}. In: {}",
+                R::METHOD,
+                msg,
+                json
+            ),
+        };
+
+        inner
+    }
+
+    async fn expect_many<R>(&mut self) -> Vec<R::Params>
+    where
+        R: tower_lsp::lsp_types::notification::Notification,
+        R::Params: serde::Serialize,
+    {
+        let mut out = Vec::new();
+
+        loop {
+            let json = match self.peek.take() {
+                Some(json) => json,
+                None => match timeout(std::time::Duration::from_millis(1000), self.receive.recv())
+                    .await
+                {
+                    Ok(Some(json)) => json,
+                    _ => break,
+                },
+            };
+
+            let Ok(res): Result<tower_lsp::jsonrpc::Request, _> = serde_json::from_str(&json)
+            else {
+                panic!("Failed to parse jsonrpc request into expected type");
+            };
+
+            let Some(value) = res.params() else {
+                self.peek = Some(json);
+                break;
+            };
+            let Ok(inner): Result<R::Params, _> = serde_json::from_value(value.clone()) else {
+                self.peek = Some(json);
+                break;
+            };
+
+            out.push(inner);
+        }
+
+        out
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut child = Command::new("../../lib/wake/lsp-wake")
-        .stdout(Stdio::piped())
-        .stdin(Stdio::piped())
-        .spawn()?;
+    let mut lsp = LSPServer::new()?;
 
-    let mut stdin = child
-        .stdin
-        .take()
-        .expect("failed to take child stdin handle");
-
-    let stdout = child
-        .stdout
-        .take()
-        .expect("failed to take child stdout handle");
-
-    let mut reader = BufReader::new(stdout).lines();
-
-    let Ok(blah) = std::env::current_dir() else {
-        todo!();
-    };
-    let Some(cwd) = blah.to_str() else {
-        todo!();
+    let cwd = std::env::current_dir()?;
+    let Some(cwd) = cwd.to_str() else {
+        panic!("Failed to convert cwd to string");
     };
 
-    let res = exchange_request_response::<Initialize>(
-        &mut stdin,
-        &mut reader,
+    lsp.write_request::<Initialize>(
         tower_lsp::jsonrpc::Id::Number(5),
         InitializeParams {
             process_id: Some(5),
@@ -180,45 +248,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     )
     .await?;
 
-    println!("{:?}", res);
+    let _response = lsp.expect_one::<Initialize>().await;
 
-    write_notification::<Initialized>(&mut stdin, InitializedParams {}).await?;
+    lsp.write_notification::<Initialized>(InitializedParams {})
+        .await?;
 
-    for n in 1..28 {
-        let res = read_notification::<PublishDiagnostics>(&mut reader).await?;
-        println!("{}: {:?}", n, res);
-    }
+    let _responses = lsp.expect_many::<PublishDiagnostics>().await;
 
     let test_wake = Url::parse(format!("file://{}/test.wake", cwd).as_str())?;
 
-    write_notification::<DidOpenTextDocument>(
-        &mut stdin,
-        DidOpenTextDocumentParams {
-            text_document: TextDocumentItem {
-                uri: test_wake.clone(),
-                language_id: "wake".to_string(),
-                version: 1,
-                text: "# comment\n# comment \n\ndef unused = 5".to_string(),
-            },
+    lsp.write_notification::<DidOpenTextDocument>(DidOpenTextDocumentParams {
+        text_document: TextDocumentItem {
+            uri: test_wake.clone(),
+            language_id: "wake".to_string(),
+            version: 1,
+            text: "# comment\n# comment \n\ndef unused = 5".to_string(),
         },
-    )
+    })
     .await?;
 
-    write_notification::<DidChangeTextDocument>(
-        &mut stdin,
-        DidChangeTextDocumentParams {
-            text_document: VersionedTextDocumentIdentifier {
-                uri: test_wake.clone(),
-                version: 1,
-            },
-            content_changes: [TextDocumentContentChangeEvent {
-                range: None,
-                range_length: None,
-                text: "# comment\n# comment \n\ndef y = 6".to_string(),
-            }]
-            .to_vec(),
+    lsp.write_notification::<DidChangeTextDocument>(DidChangeTextDocumentParams {
+        text_document: VersionedTextDocumentIdentifier {
+            uri: test_wake.clone(),
+            version: 1,
         },
-    )
+        content_changes: [TextDocumentContentChangeEvent {
+            range: None,
+            range_length: None,
+            text: "# comment\n# comment \n\ndef y = 6".to_string(),
+        }]
+        .to_vec(),
+    })
     .await?;
 
     let hover_params = HoverParams {
@@ -236,52 +296,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         },
     };
 
-    let res = exchange_request_response::<HoverRequest>(
-        &mut stdin,
-        &mut reader,
-        tower_lsp::jsonrpc::Id::Number(6),
-        hover_params.clone(),
-    )
-    .await?;
-    println!("{:?}", res);
+    lsp.write_request::<HoverRequest>(tower_lsp::jsonrpc::Id::Number(6), hover_params.clone())
+        .await?;
 
-    let res = exchange_request_response::<HoverRequest>(
-        &mut stdin,
-        &mut reader,
-        tower_lsp::jsonrpc::Id::Number(7),
-        hover_params.clone(),
-    )
-    .await?;
-    println!("{:?}", res);
+    let _response = lsp.expect_one::<HoverRequest>().await;
 
-    write_request::<HoverRequest>(
-        &mut stdin,
-        tower_lsp::jsonrpc::Id::Number(8),
-        hover_params.clone(),
-    )
-    .await?;
+    lsp.write_request::<HoverRequest>(tower_lsp::jsonrpc::Id::Number(7), hover_params.clone())
+        .await?;
 
-    for n in 1..28 {
-        let res = read_notification::<PublishDiagnostics>(&mut reader).await?;
-        println!("{}: {:?}", n, res);
-    }
+    let _response = lsp.expect_one::<HoverRequest>().await;
 
-    let res = read_response::<HoverRequest>(&mut reader).await?;
-    println!("{:?}", res);
+    lsp.write_request::<HoverRequest>(tower_lsp::jsonrpc::Id::Number(8), hover_params.clone())
+        .await?;
 
-    let res = exchange_request_response::<Shutdown>(
-        &mut stdin,
-        &mut reader,
-        tower_lsp::jsonrpc::Id::Number(9),
-        (),
-    )
-    .await?;
-    println!("{:?}", res);
+    let _responses = lsp.expect_many::<PublishDiagnostics>().await;
+    let _response = lsp.expect_one::<HoverRequest>().await;
 
-    write_notification::<Exit>(&mut stdin, ()).await?;
+    lsp.write_request::<Shutdown>(tower_lsp::jsonrpc::Id::Number(9), ())
+        .await?;
+    let _response = lsp.expect_one::<Shutdown>().await;
 
-    let status = child.wait().await?;
-    println!("child exit: {}", status);
+    lsp.write_notification::<Exit>(()).await?;
 
     Ok(())
 }
