15c23211e1f78e4e17b2105e62e90c0de85f0956
Ashley Coleman <ashley.coleman@sifive.com>
2024-03-18 09:59:37 -0700

    add test for SIGTERMd wake

---
+++ tests/inspection/canceled/pass.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+set -e
+WAKE="${1:+$1/wake}"
+
+# Start from any empty db every time for stable job ids
+rm -f wake.db
+
+# Use || true to ignore the expected non-0 return from timeout
+timeout 1 ${WAKE} test || true
+
+${WAKE} --last --canceled
