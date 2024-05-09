ae96e863d771deab9e25dff66395c07201757bf1
Ashley Coleman <ashley.coleman@sifive.com>
2024-05-09 10:40:12 -0700

    typo

--- rust/rsc/src/rsc/types.rs
+++ rust/rsc/src/rsc/types.rs
@@ -55,7 +55,7 @@ pub struct AddJobPayload {
     pub obytes: u64,
 
     // Label is not part of the job key and is not considered in any caching decisions. It is
-    // strictly use for inspecting the remote cache's database. Left as optional for soft migration
+    // strictly used for inspecting the remote cache's database. Left as optional for soft migration
     // purposes. May become required in the future
     pub label: Option<String>,
 }
