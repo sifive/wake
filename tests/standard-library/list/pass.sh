21e36e8132472f2dd950ac83c6290db701bdc6d4
Ashley Coleman <ashley.coleman@sifive.com>
2024-09-19 09:24:47 -0700

    stdlib: Add list functions

--- tests/standard-library/list/pass.sh
+++ tests/standard-library/list/pass.sh
@@ -1,4 +1,18 @@
 #! /bin/sh
 
 WAKE="${1:+$1/wake}"
-"${WAKE:-wake}" --stdout=warning,report test
+
+"${WAKE:-wake}" --stdout=warning,report test1
+"${WAKE:-wake}" --stdout=warning,report test2
+"${WAKE:-wake}" --stdout=warning,report test3
+"${WAKE:-wake}" --stdout=warning,report test4
+"${WAKE:-wake}" --stdout=warning,report test5
+"${WAKE:-wake}" --stdout=warning,report test6
+"${WAKE:-wake}" --stdout=warning,report test7
+"${WAKE:-wake}" --stdout=warning,report test8
+"${WAKE:-wake}" --stdout=warning,report test9
+"${WAKE:-wake}" --stdout=warning,report test10
+"${WAKE:-wake}" --stdout=warning,report test11
+"${WAKE:-wake}" --stdout=warning,report test12
+"${WAKE:-wake}" --stdout=warning,report test13
+"${WAKE:-wake}" --stdout=warning,report test14
